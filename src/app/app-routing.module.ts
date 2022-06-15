1dda5fc4063fbd8f4ccdb6c6ef6aee18cc478d05
Juan Saldaña <saldana.juan.2002@gmail.com>
2022-06-15 09:27:54 +0200

    More awesome changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'about', component: AboutUsComponent},
   {path: '*', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', redirectTo: ''},
 ];
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'YOURSFront'`, () => {
+  it(`should have as title 'YOURS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('YOURSFront');
+    expect(app.title).toEqual('YOURS');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('YOURSFront app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('YOURS app is running!');
   });
 });
