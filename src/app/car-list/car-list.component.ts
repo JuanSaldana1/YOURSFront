75275657e7340c80444c2ae5b9c0373623ffdd01
Juan Saldaña <saldana.juan.2002@gmail.com>
2022-06-15 14:29:07 +0200

    Updates

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-car-list',
@@ -7,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarListComponent implements OnInit {
   carList: any[] = [
-    {id: 1, name: 'BMW', price: 100},
-    {id: 2, name: 'Audi', price: 200},
-    {id: 3, name: 'Mercedes', price: 300},
-    {id: 4, name: 'Ferrari', price: 400},
-    {id: 5, name: 'Lamborghini', price: 500},
+    {
+      id: 1,
+      name: 'Cadillac Scalade 2021',
+      price: 100,
+      img: 'https://cdn.topgear.es/sites/navi.axelspringer.es/public/styles/950/public/media/image/2022/02/cadillac-escalade-2022-2612119.jpg?itok=dU926WBo',
+    },
+    {id: 2, name: 'Audi', price: 200, img: ''},
+    {id: 3, name: 'Mercedes', price: 300, img: ''},
+    {id: 4, name: 'Ferrari', price: 400, img: ''},
+    {id: 5, name: 'Lamborghini', price: 500, img: ''},
   ];
 
   constructor() {}
