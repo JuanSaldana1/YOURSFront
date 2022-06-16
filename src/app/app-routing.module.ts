6725eb607e9c7877f6a0e464ecda5248e9792e49
Juan Saldaña <saldana.juan.2002@gmail.com>
2022-06-16 10:42:18 +0200

    Heatmap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { AuthComponent } from './auth/auth.component';
 import { CarListComponent } from './car-list/car-list.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
@@ -12,7 +11,6 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'auth', component: AuthComponent},
-  {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'about', component: AboutUsComponent},
   {path: 'profile', component: ProfileComponent},
