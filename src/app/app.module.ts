69ab51730ad304ce3950252a4ff2b1c6c8cc2f07
Juan Saldaña <saldana.juan.2002@gmail.com>
2022-06-16 10:12:17 +0200

    requireds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,10 @@ import { AppComponent } from './app.component';
 import { CarListComponent } from './car-list/car-list.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 import { MenuComponent } from './shared/menu/menu.component';
-import { PrivacyComponent } from './privacy/privacy.component';
 
 @NgModule({
   declarations: [
